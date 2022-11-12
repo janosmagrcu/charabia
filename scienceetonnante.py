4eb1570b208c2745c2645b36cccd8dc9dc241bc1
janosmagrcu <jonas.magrou@psl.eu>
2022-11-12 14:22:50 +0100

    ça crée des phrases illisibles => on est 2D

--- scienceetonnante.py
+++ scienceetonnante.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-import codecs
+#import codecs
 
 
 filepath = "mots_francais.txt"
@@ -37,43 +37,47 @@ st=np.tile(s.T,(256,1,1)).T
 p=count.astype('float')/st
 p[np.isnan(p)]=0
 
+non = 12
+if non == 1:
+    # Build words
+    outfile = "output.txt"
+    f = codecs.open(outfile,"w","utf-8")
 
-# Build words
-outfile = "output.txt"
-f = codecs.open(outfile,"w","utf-8")
+    # How many for each target size
 
-# How many for each target size
+    min,max = 3,10
+    nv_mots = [[]]*(max-min)
+    K = 100
+    for TGT in range(min,max):
+        total = 0
+        while total<100:
+            i=0
+            j=0
+            res = u''
+            while not j==10:
+                k=choice(range(256),1,p=p[i,j,:])[0]
+                res = res + chr(k)
+                i=j
+                j=k
+            if len(res) == 1+TGT:
+                if res[:-1] in dico:
+                    x=res[:-1]+"*"
+                else:
+                    x=res[:-1]
+                total += 1
+                nv_mots[len(res)-1-min].append(x)
+                f.write(x+"\n")
+    f.close()
 
-min,max = 3,10
-nv_mots = [[]]*(max-min)
-K = 100
-for TGT in range(min,max):
-    total = 0
-    while total<100:
-        i=0
-        j=0
-        res = u''
-        while not j==10:
-            k=choice(range(256),1,p=p[i,j,:])[0]
-            res = res + chr(k)
-            i=j
-            j=k
-        if len(res) == 1+TGT:
-            if res[:-1] in dico:
-                x=res[:-1]+"*"
-            else:
-                x=res[:-1]
-            total += 1
-            nv_mots[len(res)-1-min].append(x)
-            f.write(x+"\n")
-f.close()
+    import random as rd
 
-import random as rd
+    phrase = '' 
+    for _ in range(20):
+        u = rd.randint(0,5-min)
+        l = rd.randint(0,10)
+        v = rd.randint(6-min,max-min-1)
+        phrase += f'{nv_mots[u][l]} {nv_mots[v][l]} '
+    print(nv_mots)
 
-phrase = '' 
-for _ in range(20):
-    u = rd.randint(0,5-min)
-    l = rd.randint(0,10)
-    v = rd.randint(6-min,max-min-1)
-    phrase += f'{nv_mots[u][l]} {nv_mots[v][l]} '
-print(nv_mots)
+text = ' '
+for _ in range(100): text = np.random.choice(letters,p=p[letters.index(text[0])][:alpha]) + text
